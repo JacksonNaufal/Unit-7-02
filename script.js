5127e1c0d6593b24b37c700a2e97237001578fc8
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-23 14:55:53 +0000

    added comments

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ let userInput = 0
 function button () {
 userInput = document.getElementById('input').value
 userInput = parseInt(userInput)
+// notices what user inputs
 
 if (userInput < 0) {
+  // Determine if written number has < 0 or is regular like 4
   document.getElementById('para').innerHTML =
   'Negative'
+  // Display Negative Text in red
   document.getElementById('para').style.color = 'red'
   } else  {
   document.getElementById('para').innerHTML = 
   'Positive'
+  // determine if number is above 0 and if so positive is written in green
   document.getElementById('para').style.color = 'green'
   }
 
