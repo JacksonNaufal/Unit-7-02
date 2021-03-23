e2dae165cca8ec18538b55a50c38a288db9a563b
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-23 15:11:39 +0000

    JS Error Fixes

--- script.js
+++ script.js
@@ -3,21 +3,20 @@ document.getElementById('button').addEventListener('click', button)
 let userInput = 0
 
 function button () {
-userInput = document.getElementById('input').value
-userInput = parseInt(userInput)
-// notices what user inputs
+  userInput = document.getElementById('input').value
+  userInput = parseInt(userInput)
+  // notices what user inputs
 
-if (userInput < 0) {
+  if (userInput < 0) {
   // Determine if written number has < 0 or is regular like 4
-  document.getElementById('para').innerHTML =
+    document.getElementById('para').innerHTML =
   'Negative'
   // Display Negative Text in red
-  document.getElementById('para').style.color = 'red'
-  } else  {
-  document.getElementById('para').innerHTML = 
+    document.getElementById('para').style.color = 'red'
+  } else {
+   document.getElementById('para').innerHTML =
   'Positive'
   // determine if number is above 0 and if so positive is written in green
-  document.getElementById('para').style.color = 'green'
+    document.getElementById('para').style.color = 'green'
   }
-
 } 
